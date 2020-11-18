62ee34628666be10203e3801063ba85a24ebeeb6
Amogh2008 <70951620+Amogh2008@users.noreply.github.com>
2020-11-18 16:47:56 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,34 @@
+var num = 45;
+console.log(typeof 55 );
+console.log(typeof "amogh");
+console.log(typeof true);
+var ob;
+console.log(ob);
+console.log(typeof ob);
+var options = {
+    isStatic : true,
+    restitution : 1.5
+
+
+}
+console.log(options);
+console.log(typeof options);
+var x = null;
+console.log(x);
+console.log(typeof x);
+var arr = [10,20,30,40,50,60,70,80,90,100]
+console.log(arr);
+console.log(typeof arr);
+console.log(arr[0]);
+console.log(arr[3]);
+console.log(arr.length);
+for(var i = 0; i<arr.length; i++){
+console.log(arr[i]);
+}
+
+var arr2 = [[10,20],[30,40],[50,60,70]]
+console.log(arr2[2][1]);
+arr2.push([666, 888, 999]);
+console.log(arr2);
+arr2.pop();
+console.log(arr2);
